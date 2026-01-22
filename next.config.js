11c23207ad200eaec701099d4dbd34b1c2ba45f6
Niko Hoogeveen <niko.hoogeveen@gmail.com>
2026-01-21 19:16:37 -0500

    Updated configs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/simcoe-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/simcoe-website' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/website' : '',
   images: {
     unoptimized: true, // Required for static export
   },
